c5abb0709af778831969641122d1a1943648db46
Colleen Rooney <crooney@pdx.edu>
2019-03-02 02:50:39 -0800

    modify scraper to save data to json file

--- backend/web_scraper.py
+++ backend/web_scraper.py
@@ -1,11 +1,26 @@
-#!./venv/bin/python3
+# Web scraping script to get data for course graph
+# 
+# example of data structure from Bryan:
+#
+# {
+# "depart": "department tag (like CS)",
+# "id": "course id with just the number, like 465",
+# "name": "course name",
+# "desc": "course description",
+# "cred": "amount of course credits",
+# "p": "true/false" [depending on if the course has P at the end of its ID, this will be cs only]
+# "pre": an array of departments and their IDs like ['CS 202', 'CS 465P']
+# }
+#
+# WORK IN PROGRESS
 from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
 import time
-import unicodedata
-import csv
+import json
+
+OUT_FILE = 'scraped_data.json'
 
 def simple_get(url):
     """
@@ -41,15 +56,30 @@ def log_error(e):
     """
     print(e)
 
-def get_data(page, math=False):
-    data = {}
-    data['courses'] = []
-    course = []
+def get_data(page, data, math=False):
     soup = BeautifulSoup(page, 'html.parser')
     cid = soup.find_all('h1')
+
     for c in cid:
-        print(c.get_text())
-        course.append(c.get_text().strip().replace(',', ''))
+        course_split = c.get_text().split(' ')
+        depart = course_split[0]
+        ID = course_split[1]
+
+        # temp, need to hadnle both courses
+        if len(ID) > 3:
+            ID = course_split[1].split('/')[1]
+        name = (' ').join(course_split[2:])
+
+    # initalize data to false
+    depart = None
+    ID = None
+    name = None
+    desc = None
+    credits = None
+    preq = None
+    p = None
+
+    # hacky
     if not math:
         tags = soup.find_all('tr')
 
@@ -57,37 +87,52 @@ def get_data(page, math=False):
             text = t.get_text()
             entries = text.split(':')
             if str(entries[0]) == 'Credit Hours':
-                course.append(
-                    entries[1].strip().replace(',', ''))
+                credits = entries[1].strip().replace(',', '')
+                # temp, need to hadnle both courses
+                if len(credits) > 1:
+                    # edge case, fix
+                    if len(entries) > 2:
+                        credits = 3
+                    else:
+                        credits = credits.split('/')[1]
             if str(entries[0]) == 'Course Description':
-                course.append(
-                    entries[1].strip().replace(',', ''))
+                desc = entries[1].strip().replace(',', '')
             if str(entries[0]) == 'Prerequisites':
-                course.append(
-                    entries[1].strip().replace(',', ''))
+                # this is going to need some cleaning
+                preq = entries[1].strip().replace(',', '')
     elif math:
         pass
-    while len(course) < 4:
-        course.append('')
-    return course
+    data['courses'].append({
+        'depart': depart,
+        'id': ID,
+        'name': name,
+        'desc': desc,
+        'cred': credits,
+        'p': p,
+        'pre': preq
+    })
+    return data
 
-def traverse_links(links, math=False):
+def traverse_links(links, data, math=False):
     pre = ''
     if math:
         pre = "http://pdx.smartcatalogiq.com/"
     for li in links:
         link = str(li).split('"')
         page = simple_get(pre + link[1])
-        data = get_data(page, math)
-        #print(data[0] + "," + data[1] + "," + data[3])
+        get_data(page, data, math)
 
-
-page = simple_get('https://www.pdx.edu/computer-science/cs570')
 
 top_page = simple_get('https://www.pdx.edu/computer-science/graduate-courses')
+# MATH - need to implement
 #top_page = simple_get('http://pdx.smartcatalogiq.com/2018-2019/Bulletin/Courses/Mth-Mathematical-Sciences')
 
 soup = BeautifulSoup(top_page, 'html.parser')
 tags = soup.find_all('ul')
 links = tags[6].find_all('li')
-traverse_links(links, math=False)
+
+data = {}
+data['courses'] = []
+traverse_links(links, data, math=False)
+with open(OUT_FILE, 'w') as outfile:
+    json.dump(data, outfile)
